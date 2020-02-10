2c15301f35716a4a6ea884f88fc58f9df5085dac
Akshat <a.sachanaaa@gmail.com>
2020-02-10 23:44:40 +0530

    Enabling storage of input values in arrays

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React,{useState} from "react";
 import Header from "./header.jsx";
 import Footer from "./footer.jsx";
 import Create from "./note.jsx";
 
 import CreateArea from "./CreateArea.jsx";
 function App() {
+  const [titarr,setTitarr]=useState([]);
+  const [dataarr,setDataarr]=useState([]);
+  function adder(title,data){
+      setTitarr(prev=>{
+        return [...prev,title];
+      });
+      setDataarr(prev=>{
+        return [...prev,data];
+      })
+      console.log(titarr);
+      console.log(dataarr);
+  }
   return (
     <div>
       <Header />
-      <CreateArea/>
+      <CreateArea funion={adder}/>
+      <Create/>
       <Footer />
     </div>
   );
--- src/CreateArea.jsx
+++ src/CreateArea.jsx
@@ -1,15 +1,43 @@
-import React from "react";
+import React,{useState} from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+ 
+  const [title,setTitle] = useState("");
+  const [data,setData]= useState("");
+  function titler(event){
+    setTitle(event.target.value);
+  }
+  function dater(event){
+    setData(event.target.value);
+  }
+  /*function adder(){
+    setTitarr(prev=>{
+      return (
+        [...prev,title]
+    );
+    });
+    setDataarr(prev=>{
+      return(
+        [...prev,data]
+      );
+    })
+  }*/
+ function submitter(event){
+    event.preventDefault();
+    props.funion(title,data);
+    setTitle("");
+    setData("");
+ }
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input name="title" placeholder="Title" onChange={titler} value={title}/>
+        <textarea name="content" placeholder="Take a note..." rows="3" onChange={dater} value={data}/>
+        <button onClick={submitter}>Add</button>
       </form>
     </div>
   );
 }
 
 export default CreateArea;
+//export {titarr,dataarr};
